dda924b62427d35f491665b04c3f0108151b22c4
Raphael Faria <raphael@codelogic.me>
2013-11-23 14:40:28 -0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function onResize() {
+  var winHeight = $(window).height();
+  $("#sec-one").height(winHeight + 20);
+  $(".logo-wrapper").css('margin-top', (winHeight - 270)/2);
+}
+
+$(function() {
+  onResize();
+  $(window).resize(function() {
+    if($(window).scrollTop() == 0) {
+      onResize();
+    }
+  });
+  $(window).scroll(function(){
+    if($(window).scrollTop() <= 0) {
+      $("header").removeClass("scroll");
+    }
+    else {
+      $("header").addClass("scroll");
+    }
+
+    if($(window).scrollTop() > $(window).height()) {
+      $("#sec-one .wrapper").addClass('inactive');
+      $("#logo").addClass('active');
+    }
+    else {
+      $("#sec-one .wrapper").removeClass('inactive');
+      $("#logo").removeClass('active');
+    }
+  });
+});
