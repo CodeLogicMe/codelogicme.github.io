1761a465b5906efa6fb307fc71fb63d611bf4498
Raphael Faria <raphael@codelogic.me>
2013-11-23 20:10:38 -0200

    Various changes in layout

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
-function onResize() {
-  var winHeight = $(window).height();
-  $("#sec-one").height(winHeight + 20);
-  $(".logo-wrapper").css('margin-top', (winHeight - 270)/2);
-}
+// function onResize() {
+//   var winHeight = $(window).height();
+//   $("#sec-one").height(winHeight + 20);
+//   $(".logo-wrapper").css('margin-top', (winHeight - 270)/2);
+// }
 
-$(function() {
-  onResize();
-  $(window).resize(function() {
-    if($(window).scrollTop() == 0) {
-      onResize();
-    }
-  });
-  $(window).scroll(function(){
-    if($(window).scrollTop() <= 0) {
-      $("header").removeClass("scroll");
-    }
-    else {
-      $("header").addClass("scroll");
-    }
+// $(function() {
+//   onResize();
+//   $(window).resize(function() {
+//     if($(window).scrollTop() == 0) {
+//       onResize();
+//     }
+//   });
+//   $(window).scroll(function(){
+//     if($(window).scrollTop() <= 0) {
+//       $("header").removeClass("scroll");
+//     }
+//     else {
+//       $("header").addClass("scroll");
+//     }
 
-    if($(window).scrollTop() > $(window).height()) {
-      $("#sec-one .wrapper").addClass('inactive');
-      $("#logo").addClass('active');
-    }
-    else {
-      $("#sec-one .wrapper").removeClass('inactive');
-      $("#logo").removeClass('active');
-    }
-  });
-});
+//     if($(window).scrollTop() > $(window).height()) {
+//       $("#sec-one .wrapper").addClass('inactive');
+//       $("#logo").addClass('active');
+//     }
+//     else {
+//       $("#sec-one .wrapper").removeClass('inactive');
+//       $("#logo").removeClass('active');
+//     }
+//   });
+// });
+
+
+// $(function() {
+
+//   $(".home-container").click(function() {
+//     $(this).addClass("inactive-section");
+//   });
+// });
