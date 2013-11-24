3af06d4e8d2a2198cb110eaf24a935f491d30464
Raphael Faria <raphael@codelogic.me>
2013-11-24 02:08:08 -0200

    Making all sections work

--- js/app.js
+++ js/app.js
@@ -3,13 +3,36 @@ $(function() {
   var hashLocation = "",
       $mainContainer = $("#main-container");
 
+  winHeight = $(window).height();
+  winWidth = $(window).width();
+
+  loadLocation = window.location.hash.substring(1);
+
+  $(".logo-image").hover(function() {
+    $(".logo-text").toggleClass("active");
+  });
+
+  if(loadLocation === "") {
+    window.location.replace("#/");
+  }
+  else {
+    console.log(loadLocation);
+    $('a[href="' + loadLocation + '"]').parent().addClass("active");
+  }
+
+
+  // $(".loading-indicator").css("top", (winHeight - 16)/2);
+  $(".loading-indicator").css("left", (winWidth - 16)/2);
+  $(".loading-indicator").addClass("is-loading");
+
+
   pageTitleContainer = $(".page-title-container");
   pageTitleContainer.removeClass("inactive");
 
-
-
   $("header").delegate("a", "click", function() {
     window.location.hash = $(this).attr("href");
+    $(".nav-list li").removeClass("active");
+    $(this).parent().addClass("active");
     return false;
   });
 
@@ -18,10 +41,13 @@ $(function() {
 
     hashLocation = window.location.hash.substring(1);
 
+
     setTimeout(function() {
       if(hashLocation) {
         $mainContainer.find("#cont").fadeOut(200, function() {
+          $(".loading-indicator").addClass("is-loading");
           $mainContainer.hide().load(hashLocation + " #cont", function() {
+            $(".loading-indicator").removeClass("is-loading");
             $mainContainer.fadeIn(200, function() {
               pageTitleContainer = $(".page-title-container");
               pageTitleContainer.removeClass("inactive");
@@ -31,6 +57,9 @@ $(function() {
       }
     }, 250);
   });
+
+  $(window).trigger('hashchange');
+
 });
 
 
